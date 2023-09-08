945c7054efd5cdb7e18abbefa49891842776f66f
unknown <hijulia259@gmail.com>
2023-09-08 16:16:17 -0300

    Photo

--- ReactJS/InstaPaw/instapaw/src/Components/Home/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from "../Home/Home.module.css"
-function Home() {
-  return (
-    <div className={styles.home}>efefvvyvyef</div>
-  )
-}
-
-export default Home
--- ReactJS/InstaPaw/instapaw/src/Components/Login/LoginPasswordReset.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function LoginReset() {
-  return (
-    <div>LoginReset</div>
-  )
-}
-
-export default LoginReset
